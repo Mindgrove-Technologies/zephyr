638e801361cd2af1d7a2945cdf0ec342415b3d24
jennifer-vinita <jennifer@mindgrovetech.in>
2025-04-04 11:21:46 +0530

    Revert "WIP XiP Flash Zephyr"
    This reverts commit 1b5da67911085b429491cd54ed48ff42e1cbed9f.

--- boards/riscv/secure-iot/board.cmake
+++ boards/riscv/secure-iot/board.cmake
@@ -2,7 +2,7 @@
 
 if(CONFIG_BOARD_SECURE_IOT)
   board_runner_args(openocd "--use-elf" "--config=${ZEPHYR_BASE}/boards/riscv/secure-iot/support/FT2232H-JTAG.cfg")
-  set(OPENOCD "/usr/local/bin/openocd")
+  set(openocd "/usr/local/bin/mind-openocd")
 
   include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 
--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,16 +6,12 @@
 
 #include <stdio.h>
 #include <stdint.h>
-// volatile uint8_t data=23;
-// uint8_t data1=34;
-// const uint32_t data2=45;
+uint8_t data=23;
+uint8_t data1=34;
+const uint32_t data2=45;
 int main(void)
 {
-	// int i = 0;
-	// uint8_t local=90;
+	uint8_t local=90;
 	printf("Hello World! %s\n", CONFIG_BOARD);
-	while(1){
-		// data = i++;
-	}
 	return 0;
 }
