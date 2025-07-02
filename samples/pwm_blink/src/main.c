62dea7747e4f1df6b4d19d681042eb97ab75726e
Harini1802 <harini@mindgrovetech.in>
2025-07-02 18:12:57 +0530

    Added get_cycles_per_sec function

--- samples/pwm_blink/src/main.c
+++ samples/pwm_blink/src/main.c
@@ -8,27 +8,18 @@ LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 #include <string.h>
 #include <zephyr/drivers/pwm.h>
 
-/* pwmcfg Bit Offsets */
-#define PWM_0 0
-#define PWM_1 1
-#define PWM_2 2
-#define PWM_3 3
-#define PWM_4 4
-#define PWM_5 5
-#define PWM_6 6
-#define PWM_7 7
+void main() 
+{
+    const struct device *dev0 = DEVICE_DT_GET(DT_NODELABEL(pwm0));
+    const struct device *dev1 = DEVICE_DT_GET(DT_NODELABEL(pwm1));
+    uint64_t cycle0,cycle1;
 
-void main() {
+    pwm_set_cycles(dev0,0, 1000, 500, PWM_POLARITY_NORMAL);
+    pwm_set_cycles(dev1,0, 100, 50, PWM_POLARITY_NORMAL);
+    
+	pwm_get_cycles_per_sec(dev0, 0, &cycle0);
+	pwm_get_cycles_per_sec(dev1, 0, &cycle1);
 
-    const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(pwm0));
-
-    pwm_set_cycles(dev,PWM_0, 1000, 750, PWM_POLARITY_NORMAL);
-    pwm_set_cycles(dev,PWM_1, 1000, 500, PWM_POLARITY_NORMAL);
-    pwm_set_cycles(dev,PWM_2, 1000, 250, PWM_POLARITY_NORMAL);
-    pwm_set_cycles(dev,PWM_3, 1000, 750, PWM_POLARITY_NORMAL);
-    pwm_set_cycles(dev,PWM_4, 1000, 750, PWM_POLARITY_NORMAL);
-    pwm_set_cycles(dev,PWM_5, 1000, 750, PWM_POLARITY_NORMAL);
-    pwm_set_cycles(dev,PWM_6, 1000, 750, PWM_POLARITY_NORMAL);
-    pwm_set_cycles(dev,PWM_7, 1000, 750, PWM_POLARITY_NORMAL);
-    printf("set\n");
+    printf("Frequency of PWM0 : Hz%u\n",cycle0);
+    printf("Frequency of PWM1 : Hz%u\n",cycle1);
 }
